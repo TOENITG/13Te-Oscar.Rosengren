52f6c2d32fbdb8f7a7db6d16585b9b9ad51ed297
Oscar Rosengren <prox074@gmail.com>
2014-10-13 18:44:00 +0200

    Klar uppgift2
    Nu är jag klar med uppgift 2 också

--- Prog1/uppgift1/src/uppgift1/uppgift2.java
+++ Prog1/uppgift1/src/uppgift1/uppgift2.java
@@ -1,15 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package uppgift1;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Oscar
- */
 public class uppgift2 {
-    
+    public static void main(String[] args) {
+        String First = JOptionPane.showInputDialog("First word");
+        String Second = JOptionPane.showInputDialog (First);
+        String Third = JOptionPane.showInputDialog (First + " " + Second);
+        String Fourth = JOptionPane.showInputDialog (First + " " + Second + " " + Third);
+        String Fifth = JOptionPane.showInputDialog (First + " " + Second + " " + Third + " " + Fourth);
+   
+        String words = First + " " + Second + " " + Third + " " + Fourth + " " + Fifth;
+        JOptionPane.showMessageDialog(null, words);
+        System.exit(0);
+    }
 }
