4b6d949dbf9a9e73b3d21a55164b787783437c45
Oscar Rosengren <prox074@gmail.com>
2014-12-17 09:11:13 +0100

    klar quiz
    har komenterat koden

--- m8/src/quiz/Quiz.java
+++ m8/src/quiz/Quiz.java
@@ -4,25 +4,32 @@ public class Quiz {
 
    
     public static void main(String[] args) {
+        
         int score = 0;
+        //this is a variable that shows the value of score in the begining.
         String start = "What's the capital of";
+        // this is the part with the start of the question.
         String[] answers = {"Bangkok","Seoul","Paris","Ottawa","Lima","Tokyo","Dublin","Moscow","Freetown","Hanoi"};
+        //this is the array for the answers.
         String[] questions = { start + " Thailand?",start + " South Korea?",start + " France?",
             start + " Canada?",start + " Peru?",start + " Japan?", start + " Ireland?",
             start + " Russia?",start + " Sierra Leone?",start + " Vietnam?"};
+        //this is the array for the questions.
         for(int i =0;i<10;i++){
+        //this makes the next question come forward after you have answerd the previous question.
             Scanner quiz = new Scanner(System.in);
             System.out.println(questions[i]);
             String answer = quiz.nextLine();
+            //asks the player the question.
             if (answer.equalsIgnoreCase(answers[i])){
                 score++;
                  System.out.println("correct");
             }else{
                 System.out.println("false");
-            }  
+            }  //tells the player if the answerd correct or false.
         }
        System.out.println("score: " + score);
-       
+       //prints out the score that the player have got.
     }
     
 }
